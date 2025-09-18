c675b4a9c54176a0b70c35b9a1ffdc8aa1275361
OUSSAMA YAAGOUB <oyaagoub@externas.sescam.jccm.es>
2025-09-18 18:08:23 +0200

    update auth-shell.component.ts

--- projects/auth/src/app/domains/auth/feature/auth-shell.component.ts
+++ projects/auth/src/app/domains/auth/feature/auth-shell.component.ts
@@ -8,8 +8,8 @@ import { BtnContinueWithFacebook } from "@domains/facebook/feature/continue-with
     imports: [BtnContinueWithGoogle, /** BtnContinueWithLinkedIn, BtnContinueWithFacebook */],
     template: `
     <div class="mfe-auth-flex mfe-auth-justify-center mfe-auth-items-center mfe-auth-min-w-screen mfe-auth-min-h-screen mfe-auth-bg-gray-100">
-        <div class="mfe-auth-flex mfe-auth-flex-col mfe-auth-gap-2 ">
-        <btn-continue-with-google class="mfe-auth-w-full md:mfe-auth-max-w-md"></btn-continue-with-google>
+        <div class="mfe-auth-flex mfe-auth-flex-col mfe-auth-gap-2 mfe-auth-w-full max-sm:mfe-auth-p-3 md:mfe-auth-max-w-md ">
+        <btn-continue-with-google></btn-continue-with-google>
         <!-- <btn-continue-with-linkedin class="mfe-auth-max-w-xs"></btn-continue-with-linkedin>
         <btn-continue-with-facebook class="mfe-auth-max-w-xs"></btn-continue-with-facebook> -->
         </div>
